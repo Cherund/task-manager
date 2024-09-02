6b8ccec5c439682bc7e622e4f9024357f70761b1
Egor Gerimovich <cherund@gmail.com>
2024-09-02 11:41:34 +0200

    add login/logout views from views.py

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,25 +1,27 @@
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.messages.views import SuccessMessageMixin
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView
+from django.utils.translation import gettext as _
+from task_manager.settings import LOGIN_REDIRECT_URL, LOGOUT_REDIRECT_URL
 
-
-# def index(request):
-#     return render(request, 'index.html')
 
 class IndexView(TemplateView):
     template_name = 'index.html'
 
 
-def users(request):
-    return render(request, 'users.html')
+class UserLoginView(SuccessMessageMixin, LoginView):
+    template_name = 'login.html'
+    form_class = AuthenticationForm
+    success_url = LOGIN_REDIRECT_URL
+    success_message = _('You have successfully logged in')
 
 
-def login(request):
-    return render(request, 'login.html')
-
-
-def logout(request):
-    return None
+class UserLogoutView(SuccessMessageMixin, LogoutView):
+    next_page = LOGOUT_REDIRECT_URL
+    success_message = _('You have successfully logged out')
 
 
 def statuses(request):
