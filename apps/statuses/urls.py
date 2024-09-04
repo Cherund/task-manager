ff926ff921696877bf93fc067bf26acda9e0fe6a
Egor Gerimovich <cherund@gmail.com>
2024-09-04 13:03:13 +0200

    add urls for delete and update views to urls.py

--- apps/statuses/urls.py
+++ apps/statuses/urls.py
@@ -4,5 +4,7 @@ from apps.statuses import views
 
 urlpatterns = [
     path('', views.StatusIndexView.as_view(), name='statuses'),
-    path('create/', views.StatusCreateView.as_view(), name='statuses_create')
+    path('create/', views.StatusCreateView.as_view(), name='statuses_create'),
+    path('<int:pk>/update/', views.StatusUpdateView.as_view(), name='statuses_update'),
+    path('<int:pk>/delete/', views.StatusDeleteView.as_view(), name='statuses_delete'),
     ]
