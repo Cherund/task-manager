05ce6abba7b0af3568c799d99c4606e1fcb8d8bc
Egor Gerimovich <cherund@gmail.com>
2024-09-08 11:36:12 +0200

    add urls.py to apps/labels

---
+++ apps/labels/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from apps.labels import views
+
+
+urlpatterns = [
+    path('', views.LabelIndexView.as_view(), name='labels'),
+    path('create/', views.LabelCreateView.as_view(), name='labels_create'),
+    path('<int:pk>/update/', views.LabelUpdateView.as_view(), name='labels_update'),
+    path('<int:pk>/delete/', views.LabelDeleteView.as_view(), name='labels_delete'),
+    ]
