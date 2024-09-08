d97ad0a8efda87fcf8413cd3df65026958160137
Egor Gerimovich <cherund@gmail.com>
2024-09-08 11:38:04 +0200

    renamed field in models.py

--- apps/statuses/models.py
+++ apps/statuses/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 class Status(models.Model):
     name = models.CharField(max_length=200)
-    timestamp = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
