1a2357628c100ab2ce341445e2f50ac49be3b634
Egor Gerimovich <cherund@gmail.com>
2024-09-16 21:02:13 +0200

    add name translation in models.py

--- apps/statuses/models.py
+++ apps/statuses/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
 
 
 # Create your models here.
 class Status(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(_('name'), max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
