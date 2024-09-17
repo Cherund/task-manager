77949c371618a0756320b26b36df2aa477d7013f
Egor Gerimovich <cherund@gmail.com>
2024-09-17 15:52:18 +0200

    delete commented sections

--- apps/users/forms.py
+++ apps/users/forms.py
@@ -7,24 +7,8 @@ class CustomUserCreationForm(UserCreationForm):
         model = get_user_model()
         fields = ('first_name', 'last_name', 'username', )
 
-    # def clean_username(self):
-    #     username = self.cleaned_data.get('username')
-    #     if self._meta.model.objects.exclude(pk=self.instance.pk). \
-    #             filter(username__iexact=username).exists():
-    #         self.add_error(
-    #             'username',
-    #             self.instance.unique_error_message(
-    #                 self._meta.model, ['username']
-    #             )
-    #         )
-    #
-    #     return username
-
 
 class CustomUserChangeForm(CustomUserCreationForm):
-    # class Meta:
-    #     model = get_user_model()
-    #     fields = ('first_name', 'last_name', 'username', 'password', )
 
     def clean_username(self):
         return self.cleaned_data.get('username')
--- apps/users/models.py
+++ apps/users/models.py
@@ -8,12 +8,3 @@ def get_full_name(self):
 
 get_user_model().add_to_class('__str__', get_full_name)
 
-# class User(models.Model):
-#     name = models.CharField(max_length=200)
-#     surname = models.CharField(max_length=200)
-#     nickname = models.CharField(max_length=200)
-#     password = models.CharField(max_length=200)
-#     timestamp = models.DateTimeField(auto_now_add=True)
-#
-#     def __str__(self):
-#         return f'{self.nickname} {self.surname}'
