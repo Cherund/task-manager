c933a7ae187e72647a9c97c54359cc0f27b0fc00
Egor Gerimovich <cherund@gmail.com>
2024-09-25 12:27:56 +0200

    remove Login, Logout views

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,26 +1,6 @@
-from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth.views import LoginView, LogoutView
-from django.contrib.messages.views import SuccessMessageMixin
 from django.views.generic import TemplateView
-from django.utils.translation import gettext as _
-from django.conf import settings
-from django.contrib import messages
 
 
 class IndexView(TemplateView):
     template_name = 'index.html'
 
-
-class UserLoginView(SuccessMessageMixin, LoginView):
-    template_name = 'login.html'
-    form_class = AuthenticationForm
-    success_url = settings.LOGIN_REDIRECT_URL
-    success_message = _('You are logged in')
-
-
-class UserLogoutView(SuccessMessageMixin, LogoutView):
-    next_page = settings.LOGOUT_REDIRECT_URL
-
-    def dispatch(self, request, *args, **kwargs):
-        messages.info(request, _('You are logged out'))
-        return super().dispatch(request, *args, **kwargs)
