b2348e78f6eab9bf3d229f9c5eff8e647957e190
Egor Gerimovich <cherund@gmail.com>
2024-09-27 12:00:51 +0200

    add mixins

--- apps/core/mixins.py
+++ apps/core/mixins.py
@@ -5,6 +5,10 @@ from django.shortcuts import redirect
 from django.urls import reverse
 from django.utils.translation import gettext as _
 
+from apps.labels.models import Label
+from apps.statuses.models import Status
+from apps.tasks.models import Task
+
 
 class CustomLoginRequiredMixin(LoginRequiredMixin):
     def handle_no_permission(self):
@@ -26,3 +30,27 @@ class SetUpLoggedUserMixin:
     def setUp(self):
         self.client.login(username=self.user.username,
                           password=self.user_data['password'])
+
+
+class SetUpLoggedUserWithLabelMixin(SetUpLoggedUserMixin):
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+        cls.label = Label.objects.create(name='Test Label')
+
+
+class SetUpLoggedUserWithStatusMixin(SetUpLoggedUserMixin):
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+        cls.status = Status.objects.create(name='Test Status')
+
+
+class SetUpLoggedUserWithTaskMixin(SetUpLoggedUserMixin):
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+        cls.status = Status.objects.create(name='Test Status')
+        cls.task = Task.objects.create(name='Test Task',
+                                       status=cls.status,
+                                       creator=cls.user)
