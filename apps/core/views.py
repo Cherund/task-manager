c72ebd1ad022dc05866f3ee1c3ecf11d605cfe12
Egor Gerimovich <cherund@gmail.com>
2024-11-04 12:04:35 +0100

    add custom_404_view to views.py in core app

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,5 +1,10 @@
+from django.shortcuts import render
 from django.views.generic import TemplateView
 
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+
+def custom_404_view(request, exception):
+    return render(request, '404.html', status=404)
